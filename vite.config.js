5139996ee77c34235c9caa6a65d2c5f600c8ea1b
Panida Paethanom <143201084+Panida287@users.noreply.github.com>
2024-12-14 18:26:39 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,17 +8,18 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   appType: "mpa",
-  base: "",
+  base: "/FED2-SP-auction-house/", // Add this to specify the base path for GitHub Pages
   build: {
     target: "esnext",
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, "./index.html"),
-        login: path.resolve(__dirname, "./auth/login/index.html"),
-        register: path.resolve(__dirname, "./auth/register/index.html"),
-        profile: path.resolve(__dirname, "./profile/index.html"),
-        listing: path.resolve(__dirname, "./listing/index.html"),
+        main: path.resolve(__dirname, "index.html"),
+        login: path.resolve(__dirname, "auth/login/index.html"),
+        register: path.resolve(__dirname, "auth/register/index.html"),
+        profile: path.resolve(__dirname, "profile/index.html"),
+        listing: path.resolve(__dirname, "listing/index.html"),
       },
     },
   },
 });
+
